44536d6f94106ed9f512f07277ddbad1567ea55d
raj donda <rajdonda1982007@gmail.com>
2025-08-31 20:35:42 +0530

    print in python

---
+++ chap2/main.py
@@ -0,0 +1,14 @@
+# this is commant in python Comments, Escape Sequences & Print Statement
+print("this is \"Escape sequences\" \nand print statement") # this is a print in python
+'''
+this
+is
+multiline
+Comments
+in
+python.
+#this is single line comment in multi line comment this not making any meaning but also not shows error.
+we can also use " instead of '
+'''
+print("hello this is","raj,","and he is",19,sep="\n",end="") # bydefualt end has \n we can give it null value by making end="" means "" has no value its null 
+print("newline")
