0a7036974352717850e77d27028f757c7b64c916
raj donda <rajdonda1982007@gmail.com>
2025-09-01 20:42:03 +0530

    more types

--- chap3/main.py
+++ chap3/main.py
@@ -6,7 +6,13 @@ c = 1.23456789
 print(c)
 d = "this","is","tuple",123
 print(d)
+D = True
+print(D)
+e = None
+print(e)
 print("type of a :" ,type(a))
 print("type of b :" ,type(b))
 print("type of c :" ,type(c))
 print("type of d :" ,type(d))
+print("type of D :" ,type(D))
+print("type of e :" ,type(e))
