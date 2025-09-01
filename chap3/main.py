d4d071760ad9494be218f3dbc9de03223a478ea0
raj donda <rajdonda1982007@gmail.com>
2025-09-01 20:37:57 +0530

    more details

--- chap3/main.py
+++ chap3/main.py
@@ -4,5 +4,9 @@ b = "this is a string"
 print(b)
 c = 1.23456789
 print(c)
+d = "this","is","tuple",123
+print(d)
 print("type of a :" ,type(a))
 print("type of b :" ,type(b))
+print("type of c :" ,type(c))
+print("type of d :" ,type(d))
