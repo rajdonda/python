703a5dfd0da776b609ddd9b254b82c07bdd37344
raj donda <rajdonda1982007@gmail.com>
2025-09-01 20:57:54 +0530

    keyandvalues

--- chap3/main.py
+++ chap3/main.py
@@ -12,8 +12,10 @@ e = None
 print(e)
 f =complex(a,c)
 print(f)
-g = [1 ,-5 ,0.7,"listitem" ,[4,-2] ,complex(a,c)]
+g = [1 ,-5 ,0.7,"listitem" ,[4,-2] ,complex(a,c) ,True ,False ,["anotherlist" ,complex(a,c)] ]
 print(g)
+h = {name:"raj",surname="donda",age=19}
+print(h)
 print("type of a :" ,type(a))
 print("type of b :" ,type(b))
 print("type of c :" ,type(c))
@@ -22,3 +24,4 @@ print("type of D :" ,type(D))
 print("type of e :" ,type(e))
 print("type of f :" ,type(f))
 print("type of g :" ,type(g))
+print("type of h :" ,type(h))
