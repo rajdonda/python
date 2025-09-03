d7df407256786a37e16c5a3965dcbe6a1200e701
raj donda <rajdonda1982007@gmail.com>
2025-09-03 21:09:28 +0530

    float or int?

--- chap5/main.py
+++ chap5/main.py
@@ -5,8 +5,13 @@ print(a + b)
 print(int(a) + int(b))
 print(str(a) + str(b))
 
-string = "15ewdfh"
+string = "15"
 num = 7
 string_number = int(string)
 sum = num + string_number
 print(sum)
+
+c = 1.8
+d = 8
+
+print(c + d)
