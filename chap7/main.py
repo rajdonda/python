08ea75b1cdf9d67fde8216e27024ebabcf9f445e
raj donda <rajdonda1982007@gmail.com>
2025-09-05 23:29:29 +0530

    adding string into print

--- chap7/main.py
+++ chap7/main.py
@@ -2,5 +2,16 @@ name = "raj"
 gmail = 'rajdonda1982007@gmail.com'
 password = "raj@1982007"
 message = "the email is correct "  "but \"password\" isnt."
+st = '''
+    this is like string comment 
+    works on multiple lines
+    another line
+    more lines
+    one more line
+    line end
 
+'''
 print(message)
+print(st)
+print("name is :" ,name)
+print("email is :" + gmail)
